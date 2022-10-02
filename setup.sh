fda01f9470795ae61e03c7063da81073f6586a2b
Albert Garde <albertsgarde@gmail.com>
2022-10-02 16:03:35 +0200

    :poop:

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 cd $1/audio_samples_py;
 pwd
 ls
-apt-get -y install cargo
+apt-get -y install cargo > ../setup.log ;
 export PATH=$PATH:$HOME/.cargo/bin
-pip install maturin;
+pip install maturin >> ../setup.log ;
 echo "maturin installed"
-maturin build --sdist;
+maturin build --sdist >> ../setup.log ;
 echo "package built"
 pwd
 ls
-pip install target/wheels/*;
+pip install target/wheels/* >> ../setup.log ;
