c1e3a7848942cd3b759067e62d53a5049525ec3d
Albert Garde <albertsgarde@gmail.com>
2022-10-02 16:00:18 +0200

    :poop:

--- setup.sh
+++ setup.sh
@@ -4,7 +4,9 @@ ls
 apt-get -y install cargo
 export PATH=$PATH:$HOME/.cargo/bin
 pip install maturin;
+echo "maturin installed"
 maturin build --sdist;
+echo "package built"
 pwd
 ls
 pip install target/wheels/*;
