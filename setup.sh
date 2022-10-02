0558bc16d329c089dc606b4f573cd7c6ba59a424
Albert Garde <albertsgarde@gmail.com>
2022-10-02 15:53:11 +0200

    :poop:

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 cd $1/audio_samples_py;
-apt-get -y install cargo;
+pwd
+ls
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 pip install maturin;
 maturin build --sdist;
+pwd
+ls
 pip install target/wheels/*;
