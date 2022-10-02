bde6744334c832f1a40b38154384d8c037f597f0
Albert Garde <albertsgarde@gmail.com>
2022-10-02 16:13:06 +0200

    :poop:

--- setup.sh
+++ setup.sh
@@ -3,13 +3,17 @@ pwd
 ls
 apt-get -y install rustc &> ../../setup.log ;
 echo "rustc installed"
+echo "rustc installed" >> ../../setup.log ;
 apt-get -y install cargo &> ../../setup.log ;
-echo "cargo installed"
+echo "cargo installed" 
+echo "cargo installed" >> ../../setup.log ;
 export PATH=$PATH:$HOME/.cargo/bin
 pip install maturin >> ../../setup.log 2>&1 ;
 echo "maturin installed"
+echo "maturin installed" >> ../../setup.log ;
 maturin build --sdist >> ../../setup.log 2>&1 ;
 echo "package built"
+echo "package built" >> ../../setup.log ;
 pwd
 ls
 pip install target/wheels/* >> ../../setup.log 2>&1 ;
