5d9fc7e9b44cfde8b11c4663a1d3cc96c81e2744
Albert Garde <albertsgarde@gmail.com>
2022-10-02 19:29:57 +0200

    :poop:

--- setup.sh
+++ setup.sh
@@ -1,26 +1,37 @@
-mkdir -p downloads;
-cd downloads;
-wget https://static.rust-lang.org/dist/rust-1.64.0-x86_64-unknown-linux-gnu.tar.gz;
-tar -xzf rust-1.64.0-x86_64-unknown-linux-gnu.tar.gz;
-./rust-1.64.0-x86_64-unknown-linux-gnu/install.sh;
-cd ..;
-cd $1/audio_samples_py;
-pwd
-ls
+LOG_PATH=../../setup.log;
 
-#apt-get -y install rustc &> ../../setup.log ;
-#echo "rustc installed"
-#echo "rustc installed" > ../../setup.log ;
-#apt-get -y install cargo &> ../../setup.log ;
-#echo "cargo installed" 
-#echo "cargo installed" >> ../../setup.log ;
-#export PATH=$PATH:$HOME/.cargo/bin
-pip install maturin >> ../../setup.log 2>&1 ;
-echo "maturin installed"
-echo "maturin installed" >> ../../setup.log ;
-maturin build --sdist >> ../../setup.log 2>&1 ;
-echo "package built"
-echo "package built" >> ../../setup.log ;
-pwd
-ls
-pip install --force-reinstall target/wheels/*.whl >> ../../setup.log 2>&1 ;
+echo "" > $LOG_PATH;
+
+if hash cargo &> /dev/null
+then 
+    echo "Rust already installed." | tee $LOG_PATH;
+else 
+    mkdir -p downloads;
+    cd downloads;
+    echo "Installing Rust..." | tee $LOG_PATH;
+    wget https://static.rust-lang.org/dist/rust-1.64.0-x86_64-unknown-linux-gnu.tar.gz >> $LOG_PATH 2>&1;;
+    tar -xzf rust-1.64.0-x86_64-unknown-linux-gnu.tar.gz >> $LOG_PATH 2>&1;;
+    ./rust-1.64.0-x86_64-unknown-linux-gnu/install.sh >> $LOG_PATH 2>&1;;
+    cd ..;
+    rm -rf downloads;
+    echo "Rust installed." | tee $LOG_PATH;
+fi
+
+cd $1/audio_samples_py;
+if pip list | grep -F maturin &> /dev/null
+then 
+    echo "Maturin already installed" | tee $LOG_PATH;
+else 
+    echo "Installing maturin..." | tee $LOG_PATH;
+    pip install maturin >> $LOG_PATH 2>&1;
+    echo "Maturin installed." | tee $LOG_PATH;
+    
+fi
+
+echo "Building audio_samples_py..." | tee $LOG_PATH;
+maturin build --release >> $LOG_PATH 2>&1;
+echo "audio_samples_py built." | tee $LOG_PATH;
+
+echo "Installing audio_samples_py..." | tee $LOG_PATH;
+pip install --force-reinstall target/wheels/*.whl >> $LOG_PATH 2>&1;
+echo "audio_samples_py installed." | tee $LOG_PATH;
