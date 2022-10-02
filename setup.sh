cd3dea00c8e745e3aa27c96374f0f92348ea82e4
Albert Garde <albertsgarde@gmail.com>
2022-10-02 16:11:24 +0200

    :poop:

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 cd $1/audio_samples_py;
 pwd
 ls
-apt-get -y install cargo &> ../setup.log ;
+apt-get -y install rustc &> ../../setup.log ;
+apt-get -y install cargo &> ../../setup.log ;
 export PATH=$PATH:$HOME/.cargo/bin
-pip install maturin >> ../setup.log 2>&1 ;
+pip install maturin >> ../../setup.log 2>&1 ;
 echo "maturin installed"
-maturin build --sdist >> ../setup.log 2>&1 ;
+maturin build --sdist >> ../../setup.log 2>&1 ;
 echo "package built"
 pwd
 ls
-pip install target/wheels/* >> ../setup.log 2>&1 ;
+pip install target/wheels/* >> ../../setup.log 2>&1 ;
