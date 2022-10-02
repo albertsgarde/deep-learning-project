04ff88df4a4da5ecb3e26dc3581b2d5d5659da81
Albert Garde <albertsgarde@gmail.com>
2022-10-02 19:12:10 +0200

    :poop:

--- audio_samples_py/src/lib.rs
+++ audio_samples_py/src/lib.rs
@@ -6,9 +6,66 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+#[pyclass]
+pub struct Audio {
+    samples: Vec<f32>,
+    sample_rate: u32,
+}
+
+#[pymethods]
+impl Audio {
+    fn get_sample_rate(&self) -> u32 {
+        self.sample_rate
+    }
+
+    fn get_samples(&self) -> Vec<f32> {
+        self.samples.clone()
+    }
+}
+
+impl From<audio_samples::Audio> for Audio {
+    fn from(audio: audio_samples::Audio) -> Self {
+        Self {
+            samples: audio.samples,
+            sample_rate: audio.sample_rate,
+        }
+    }
+}
+#[pyclass]
+pub struct DataGenerator {
+    generator: audio_samples::DataGenerator,
+}
+
+#[pymethods]
+impl DataGenerator {
+    #[new]
+    fn new() -> Self {
+        let data_parameters = audio_samples::DataParameters {
+            sample_rate: 44100,
+            num_samples: 256,
+            frequency_range: (20., 20000.),
+        };
+        Self {
+            generator: audio_samples::DataGenerator::new(data_parameters, 0),
+        }
+    }
+
+    fn next(&mut self) -> Audio {
+        self.generator
+            .next()
+            .unwrap()
+            .unwrap()
+            .audio()
+            .clone()
+            .into()
+    }
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn audio_samples_py(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<Audio>()?;
+    m.add_class::<DataGenerator>()?;
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
